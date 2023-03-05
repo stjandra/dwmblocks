c03523d5fd0e3181b02dca985b6040ad1cc5eaf0
Stephen Alexandro Tjandra <stephen_tjandra@hotmail.com>
2023-03-05 19:40:42 +1100

    Add vpn block. Update date block

--- blocks.h
+++ blocks.h
@@ -1,9 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+
+    // This is updated by a script in /etc/NetworkManager/dispatcher.d
+    {"",     "vpn_ip",                                                   0,    11},
+
 	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    // Datetime is updated by dwmblocks_my script every minute
+    {"",     "my_date",                                                  0,    10},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
