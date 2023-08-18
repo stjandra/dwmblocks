f6488d766d1a023cbf43f81e7798599990c8f32e
Stephen Alexandro Tjandra <stephen_tjandra@hotmail.com>
2023-08-19 02:57:59 +1000

    Add battery block. Indentation

--- blocks.h
+++ blocks.h
@@ -1,11 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+    /*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
     // This is updated by a script in /etc/NetworkManager/dispatcher.d
     {"",     "vpn_ip",                                                   0,    11},
 
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+    {"",     "my_battery",                                              30,	    0},
+
+    {"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
 
     // Datetime is updated by dwmblocks_my script every minute
     {"",     "my_date",                                                  0,    10},
